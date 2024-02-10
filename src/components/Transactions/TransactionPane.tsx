c4e718a4b990d6dc81baed4ddb0687c41c290fc0
Pavan-Soma-Shekar <shekar.p@northeastern.edu>
2024-02-10 17:13:58 -0500

    KaiznTree Front-End Take Home Challenge

---
+++ src/components/Transactions/TransactionPane.tsx
@@ -0,0 +1,38 @@
+import { InputCheckbox } from "../InputCheckbox"
+import { TransactionPaneComponent } from "./types"
+
+export const TransactionPane: TransactionPaneComponent = ({
+  transaction,
+  approved,
+  loading,
+  setTransactionApproval: consumerSetTransactionApproval,
+}) => {
+
+  return (
+    <div className="KaizntreePane">
+      <div className="KaizntreePane--content">
+        <p className="KaizntreeText">{transaction.merchant} </p>
+        <b>{moneyFormatter.format(transaction.amount)}</b>
+        <p className="KaizntreeText--hushed KaizntreeText--s">
+          {transaction.employee.firstName} {transaction.employee.lastName} - {transaction.date}
+        </p>
+      </div>
+      <InputCheckbox
+        id={transaction.id}
+        checked={approved !== undefined ? approved : transaction.approved}
+        disabled={loading}
+        onChange={async (newValue) => {
+          await consumerSetTransactionApproval({
+            transactionId: transaction.id,
+            newValue,
+          })
+        }}
+      />
+    </div>
+  )
+}
+
+const moneyFormatter = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+})
